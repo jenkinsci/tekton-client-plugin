7a31d118b84a7a686c91139365a521e772d217df
Gareth Evans <gareth@bryncynfelin.co.uk>
2021-01-05 11:27:18 +0000

    chore(deps): bump tekton-client from 4.10.3 to 5.0.0

--- src/main/java/org/waveywaves/jenkins/plugins/tekton/client/build/BaseStep.java
+++ src/main/java/org/waveywaves/jenkins/plugins/tekton/client/build/BaseStep.java
@@ -6,7 +6,6 @@ import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 import io.fabric8.tekton.client.TektonClient;
 import io.fabric8.tekton.pipeline.v1beta1.*;
-import io.fabric8.tekton.resource.v1alpha1.DoneablePipelineResource;
 import io.fabric8.tekton.resource.v1alpha1.PipelineResource;
 import io.fabric8.tekton.resource.v1alpha1.PipelineResourceList;
 import jenkins.tasks.SimpleBuildStep;
@@ -16,15 +15,15 @@ public abstract class BaseStep extends Builder implements SimpleBuildStep {
     protected transient Client tektonClient;
     protected transient Client kubernetesClient;
 
-    protected MixedOperation<TaskRun, TaskRunList, DoneableTaskRun, Resource<TaskRun, DoneableTaskRun>>
+    protected MixedOperation<TaskRun, TaskRunList, Resource<TaskRun>>
             taskRunClient;
-    protected MixedOperation<Task, TaskList, DoneableTask, Resource<Task, DoneableTask>>
+    protected MixedOperation<Task, TaskList, Resource<Task>>
             taskClient;
-    protected MixedOperation<Pipeline, PipelineList, DoneablePipeline, Resource<Pipeline, DoneablePipeline>>
+    protected MixedOperation<Pipeline, PipelineList, Resource<Pipeline>>
             pipelineClient;
-    protected MixedOperation<PipelineRun, PipelineRunList, DoneablePipelineRun, Resource<PipelineRun, DoneablePipelineRun>>
+    protected MixedOperation<PipelineRun, PipelineRunList, Resource<PipelineRun>>
             pipelineRunClient;
-    protected MixedOperation<PipelineResource, PipelineResourceList, DoneablePipelineResource, Resource<PipelineResource, DoneablePipelineResource>>
+    protected MixedOperation<PipelineResource, PipelineResourceList, Resource<PipelineResource>>
             pipelineResourceClient;
 
     public enum InputType {
@@ -42,27 +41,27 @@ public abstract class BaseStep extends Builder implements SimpleBuildStep {
     }
 
     public void setTaskRunClient(
-            MixedOperation<TaskRun, TaskRunList, DoneableTaskRun, Resource<TaskRun, DoneableTaskRun>> trc){
+            MixedOperation<TaskRun, TaskRunList, Resource<TaskRun>> trc){
         this.taskRunClient = trc;
     }
 
     public void setTaskClient(
-            MixedOperation<Task, TaskList, DoneableTask, Resource<Task, DoneableTask>> tc){
+            MixedOperation<Task, TaskList, Resource<Task>> tc){
         this.taskClient = tc;
     }
 
     public void setPipelineClient(
-            MixedOperation<Pipeline, PipelineList, DoneablePipeline, Resource<Pipeline, DoneablePipeline>> pc){
+            MixedOperation<Pipeline, PipelineList, Resource<Pipeline>> pc){
         this.pipelineClient = pc;
     }
 
     public void setPipelineRunClient(
-            MixedOperation<PipelineRun, PipelineRunList, DoneablePipelineRun, Resource<PipelineRun, DoneablePipelineRun>> prc){
+            MixedOperation<PipelineRun, PipelineRunList, Resource<PipelineRun>> prc){
         this.pipelineRunClient = prc;
     }
 
     public void setPipelineResourceClient(
-            MixedOperation<PipelineResource, PipelineResourceList, DoneablePipelineResource, Resource<PipelineResource, DoneablePipelineResource>> presc){
+            MixedOperation<PipelineResource, PipelineResourceList, Resource<PipelineResource>> presc){
         this.pipelineResourceClient = presc;
     }
 }
