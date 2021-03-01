1cc0aa2baaf444be1d9ed6364418269b83df8892
James Strachan <james.strachan@gmail.com>
2021-03-01 12:34:41 +0000

    fix: log tailing
    the previous code didn't work for me as there is often a delay between creating a PipelineRun and the actual TaskRun instances being created by the operator and starting. So added a little loop waiting for them to show up and added a bit more diagnostics in the logging

--- src/main/java/org/waveywaves/jenkins/plugins/tekton/client/logwatch/PipelineRunLogWatch.java
+++ src/main/java/org/waveywaves/jenkins/plugins/tekton/client/logwatch/PipelineRunLogWatch.java
@@ -1,18 +1,13 @@
 package org.waveywaves.jenkins.plugins.tekton.client.logwatch;
 
-import io.fabric8.knative.internal.pkg.apis.Condition;
 import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.tekton.client.TektonClient;
 import io.fabric8.tekton.pipeline.v1beta1.*;
-import net.sf.ezmorph.array.BooleanObjectArrayMorpher;
-import org.waveywaves.jenkins.plugins.tekton.client.TektonUtils.TektonResourceType;
 
 import java.io.OutputStream;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CountDownLatch;
 import java.util.logging.Logger;
 
 public class PipelineRunLogWatch implements Runnable {
@@ -43,27 +38,49 @@ public class PipelineRunLogWatch implements Runnable {
         List<PipelineTask> pipelineTasks = pipelineRun.getSpec().getPipelineSpec().getTasks();
 
         for (PipelineTask pt: pipelineTasks){
+            String pipelineTaskName = pt.getName();
+            logger.info("streaming logs for PipelineTask " + pipelineRunName + "/" + pipelineTaskName);
             ListOptions lo = new ListOptions();
-            lo.setLabelSelector(String.format("%s=%s",pipelineTaskLabelName, pt.getName()));
-            lo.setLabelSelector(String.format("%s=%s", pipelineRunLabelName, pipelineRunName));
+            String selector = String.format("%s=%s,%s=%s", pipelineTaskLabelName, pipelineTaskName, pipelineRunLabelName, pipelineRunName);
+            lo.setLabelSelector(selector);
 
-            List<TaskRun> taskRunList = tektonClient.v1beta1().taskRuns().list(lo).getItems();
-            for (TaskRun tr: taskRunList) {
-                List<OwnerReference> ownerReferences = tr.getMetadata().getOwnerReferences();
-                for (OwnerReference or : ownerReferences) {
-                    if (or.getUid().equals(pipelineRunUid)){
-                        logger.info(String.format("Streaming logs for TaskRun %s owned by PipelineRun %s", tr.getMetadata().getName(), pipelineRunName));
-                        TaskRunLogWatch logWatch = new TaskRunLogWatch(kubernetesClient, tr, consoleLogger);
-                        Thread logWatchTask = new Thread(logWatch);
-                        logWatchTask.start();
-                        try {
-                            logWatchTask.join();
-                        } catch (InterruptedException exception) {
-                            exception.printStackTrace();
+            // the tekton operator may not have created the TasksRuns yet so lets wait a little bit for them to show up
+            for (int i = 0; i < 60; i++) {
+                boolean taskComplete = false;
+                List<TaskRun> taskRunList = tektonClient.v1beta1().taskRuns().list(lo).getItems();
+                for (TaskRun tr : taskRunList) {
+                    String trName = tr.getMetadata().getName();
+                    logger.info("streaming logs for TaskRun " + trName);
+
+                    List<OwnerReference> ownerReferences = tr.getMetadata().getOwnerReferences();
+                    for (OwnerReference or : ownerReferences) {
+                        if (or.getUid().equals(pipelineRunUid)) {
+                            logger.info(String.format("Streaming logs for TaskRun %s owned by PipelineRun %s with selector %s", trName, pipelineRunName, selector));
+                            TaskRunLogWatch logWatch = new TaskRunLogWatch(kubernetesClient, tr, consoleLogger);
+                            Thread logWatchTask = new Thread(logWatch);
+                            logWatchTask.start();
+                            try {
+                                logWatchTask.join();
+                            } catch (InterruptedException exception) {
+                                exception.printStackTrace();
+                            }
+                            logger.info("TaskRun " + trName + " completed");
+                            taskComplete = true;
                         }
                     }
                 }
-
+                if (taskComplete) {
+                    logger.info("completed PipelineTask " + pipelineTaskName);
+                    break;
+                } else {
+                    logger.info("could not find OwnerReference for " + pipelineRunUid);
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // ignore
+                    e.printStackTrace();
+                }
             }
         }
     }
