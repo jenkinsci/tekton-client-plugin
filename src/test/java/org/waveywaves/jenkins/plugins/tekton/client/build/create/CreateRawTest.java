b9c5ba5904ac9b9db0dcdc225623565a5d1e9294
Gareth Evans <gareth@bryncynfelin.co.uk>
2021-05-04 22:21:23 +0100

    chore: set extra parameters during build

--- src/test/java/org/waveywaves/jenkins/plugins/tekton/client/build/create/CreateRawTest.java
+++ src/test/java/org/waveywaves/jenkins/plugins/tekton/client/build/create/CreateRawTest.java
@@ -3,8 +3,11 @@ package org.waveywaves.jenkins.plugins.tekton.client.build.create;
 import hudson.EnvVars;
 import hudson.FilePath;
 import hudson.model.Run;
-import hudson.model.TaskListener;
-import io.jenkins.plugins.checks.api.ChecksPublisher;
+import io.fabric8.tekton.pipeline.v1beta1.Param;
+import io.fabric8.tekton.pipeline.v1beta1.PipelineRun;
+import io.fabric8.tekton.pipeline.v1beta1.PipelineRunBuilder;
+import java.util.List;
+import java.util.Optional;
 import org.junit.Test;
 import org.waveywaves.jenkins.plugins.tekton.client.TektonUtils;
 import org.waveywaves.jenkins.plugins.tekton.client.build.create.mock.CreateRawMock;
@@ -14,6 +17,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CreateRawTest {
 
@@ -100,6 +106,43 @@ public class CreateRawTest {
                 "labels:\n" +
                 "  cheese: " + cheese + "\n";
         assertThat(created).isEqualTo(expectedYaml);
+    }
+
+    @Test
+    public void testEnhancePipelineWithParams() {
+        PipelineRun pr = new PipelineRunBuilder()
+                .withNewSpec()
+                    .withParams()
+                .endSpec()
+            .build();
+        assertThat(pr, is(notNullValue()));
+        assertThat(pr.getSpec(), is(notNullValue()));
+        assertThat(pr.getSpec().getParams(), is(notNullValue()));
+
+        EnvVars envVars = new EnvVars();
+        envVars.put("GIT_URL" , "https://github.com/org/repo.git");
+        envVars.put("JOB_NAME" , "test-tekton-client/main");
+        envVars.put("BUILD_ID" , "12");
+        envVars.put("GIT_COMMIT", "e9f3c472fea4661b2142c5e88928c5a35e03f51f");
+        envVars.put("GIT_BRANCH" , "main");
+
+        new CreateRaw("", "YAML").enhancePipelineRunWithEnvVars(pr, envVars);
+
+        List<Param> params = pr.getSpec().getParams();
+        assertThat(params.stream().filter( p -> p.getName().equals("REPO_URL")).findFirst().get(), is(notNullValue()));
+        assertThat(getStringValue(params, "REPO_URL"), is("https://github.com/org/repo.git"));
+        assertThat(getStringValue(params, "JOB_NAME"), is("test-tekton-client/main"));
+        assertThat(getStringValue(params, "BUILD_ID"), is("12"));
+        assertThat(getStringValue(params, "PULL_PULL_SHA"), is("e9f3c472fea4661b2142c5e88928c5a35e03f51f"));
+        assertThat(getStringValue(params, "PULL_BASE_REF"), is("main"));
+        assertThat(getStringValue(params, "REPO_NAME"), is("repo"));
+        assertThat(getStringValue(params, "REPO_OWNER"), is("org"));
+    }
+
+    private String getStringValue(List<Param> params, String name) {
+        Param param = params.stream().filter(p -> p.getName().equals(name)).findFirst().get();
+        assertThat(param, is(notNullValue()));
+        return param.getValue().getStringVal();
     }
 
 }
