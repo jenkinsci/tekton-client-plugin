58bc7670b5a9a2a6bb3f58c3c9deda3b64901a15
Abhinav R Jha <bt23csd053@iiitn.ac.in>
2025-03-20 11:43:51 +0530

    Update TektonArg.java
    Ensured value is non-null (Objects.requireNonNull).
    Used @Exported to expose the value in the Jenkins API.
    Implemented toString, equals, and hashCode for better debugging and equality checking.
    Added @Symbol("tektonArg") for better pipeline script integration.

--- src/main/java/org/waveywaves/jenkins/plugins/tekton/client/build/create/TektonArg.java
+++ src/main/java/org/waveywaves/jenkins/plugins/tekton/client/build/create/TektonArg.java
@@ -4,24 +4,48 @@ import hudson.Extension;
 import hudson.model.AbstractDescribableImpl;
 import hudson.model.Descriptor;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.export.Exported;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import java.util.Objects;
+import org.jenkinsci.Symbol;
 
 public final class TektonArg extends AbstractDescribableImpl<TektonArg> {
     private final String value;
 
     @DataBoundConstructor
-    public TektonArg(String value) {
-        this.value = value;
+    public TektonArg(@NonNull String value) {
+        this.value = Objects.requireNonNull(value, "value must not be null");
     }
 
+    @Exported
     public String getValue() {
         return value;
     }
-    
+
+    @Override
+    public String toString() {
+        return "TektonArg{" + "value='" + value + '\'' + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        TektonArg tektonArg = (TektonArg) obj;
+        return Objects.equals(value, tektonArg.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
     @Extension
+    @Symbol("tektonArg")
     public static class DescriptorImpl extends Descriptor<TektonArg> {
         @Override
         public String getDisplayName() {
-            return "arg";
+            return "Tekton Argument";
         }
     }
 }
